242e9e911a82c56c435fce43bfa6185c75f45809
Lien-Chu Huang <liarn2001@gmail>
2022-05-18 08:40:06 +0200

    Fixed growing big icons

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+const reactContentRoot = document.getElementById("root")
+
+const App = () => {
+
+   return(
+       <input type="text" id="name" placeholder="Your Name" />
+  )
+}
+
+const myJSXElement = (
+    
+    <input type="text" id="name" placeholder="Your Name" />
+ 
+)
+
+ReactDOM. render(<App /> , reactContentRoot)
